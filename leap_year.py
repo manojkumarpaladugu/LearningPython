51813dc315a06f67a48dfc8e326806a8c06ad604
manojkumarpaladugu <35074754+manojkumarpaladugu@users.noreply.github.com>
2018-08-17 09:33:31 +0530

    Delete leap_year.py

--- leap_year.py
+++
@@ -1,7 +0,0 @@
-#Python Program to Check Whether a Given Year is a Leap Year
-
-year = input("Enter a year:")
-if((year%4==0 and year%100!=0) or year%400==0):
-  print("%d is leap year" %(year))
-else:
-  print("%d is not leap year" %(year))
