725b1ecab753c3c6a02e80d7b9d29f7b9bcba0ab
manojkumarpaladugu <35074754+manojkumarpaladugu@users.noreply.github.com>
2018-08-17 09:33:17 +0530

    Delete join_strings.py

--- join_strings.py
+++
@@ -1,6 +0,0 @@
-#Python Program to Form a New String Made of the First 2 and Last 2 characters From a Given String
-
-
-string = input("Enter a string:")
-new_string = string[0:2] + string[len(string)-2:]
-print(new_string)
