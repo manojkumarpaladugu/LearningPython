851f58da004962f1ca40ae0fa2507d9404706071
manojkumarpaladugu <35074754+manojkumarpaladugu@users.noreply.github.com>
2018-08-17 09:34:43 +0530

    Delete recursion_sum_n.py

--- recursion_sum_n.py
+++
@@ -1,9 +0,0 @@
-#Write a recursive Python function that returns the sum of the first n integers.
-
-def sum_n(n):
-    if(n == 1):
-        return 1 
-    return n + sum_n(n-1)
-
-n = input("Input a number:")
-print("The sum of first {0} integers is: {1}" .format(n,sum_n(n))) 
