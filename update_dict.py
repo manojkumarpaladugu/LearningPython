6841b5f4aa0cd4f43750fdb3e31c96a93dd91399
manojkumarpaladugu <35074754+manojkumarpaladugu@users.noreply.github.com>
2018-08-17 09:35:57 +0530

    Delete update_dict.py

--- update_dict.py
+++
@@ -1,8 +0,0 @@
-#Python Program to Add a Key-Value Pair to the Dictionary
-
-key = input("Enter the key:")
-value = input("Enter the value:")
-
-dictionary = {}
-dictionary.update({key:value})
-print(dictionary)
