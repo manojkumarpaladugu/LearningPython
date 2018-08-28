fb9e05a92dbdf39f762c7f1a59073d9587a24bfa
manojkumarpaladugu <35074754+manojkumarpaladugu@users.noreply.github.com>
2018-08-28 17:09:30 +0530

    Add files via upload

---
+++ Practice Programs/GeeksforGeeks Archive/split_join_string.py
@@ -0,0 +1,10 @@
+# Python program to split and join a string
+
+my_string = input("Enter a string:")
+my_string = list(my_string)
+print my_string
+
+my_string = ''.join(my_string)
+print my_string
+
+
